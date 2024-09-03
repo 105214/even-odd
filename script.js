cbb5e9d82e6d067ed913571747a80fd0e13ae473
Nithin sukumar <nithinsukumar200@gmail.com>
2024-09-03 19:07:38 +0530

    evenorodd

---
+++ script.js
@@ -0,0 +1,13 @@
+
+num=99;
+
+let evenodd=()=>{
+    if(num%2==0){
+        ev=num;
+        console.log(ev+" "+"it is even number");
+    }
+    else {
+        od=num;
+        console.log(od+" "+"its odd number");
+    }
+}
